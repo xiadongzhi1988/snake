ab6b09423b4d2f5a9b2d28bb11f2214c9ab80410
mirreal <zgkm181142521@gmail.com>
2014-04-02 22:15:36 +0800

    像是条蛇了

--- snake.js
+++ snake.js
@@ -23,20 +23,28 @@ function canvasApp() {
 	var y = 60;
 	var dx = 0;
 	var dy = 0;
-	var snakeinitLength = 3;
+	var snakeinitLength = 4;
 	var snake = new Array();
 
-	snake = [{x: 20, y: 60}, {x: 40, y: 60}, {x: 60, y: 60}];
+	snake = [{x: 20, y: 60}, {x: 40, y: 60}, {x: 60, y: 60}, {x: 80, y: 60}];
 
 	function drawScreen() {	
 		drawGrid(context, "lightgrey",snakeLength, snakeHeight);
 		context.font = "30px Arial";
 		context.fillStyle = "#cd2828";
+		/*
 		x += dx;
 		y += dy;
 		context.fillRect(x,y,snakeLength,snakeHeight);
 		context.fillRect(x-snakeLength,y,snakeLength,snakeHeight);
 		context.fillRect(x-2*snakeLength,y,snakeLength,snakeHeight);
+		*/
+		snakeBodyMove();
+		snake[snake.length-1].x += dx;
+		snake[snake.length-1].y += dy;
+		for (var i = 0; i < snake.length-1; i++) {
+			context.fillRect(snake[i].x, snake[i].y, 20, 20);
+		}
 		gameover();
 	}
 	
@@ -46,14 +54,16 @@ function canvasApp() {
 			snake[j].x = snake[j+1].x;
 			snake[j].y = snake[j+1].y; 
 		}
+		/*
 		snake[snakeinitLength-1] = {
 			x: snake[snakeinitLength-2].x + dx,
 			y: snake[snakeinitLength-2].y + dy
-		}; 	
+		};
+		*/ 	
 	}
 		
 	window.onkeydown=processkey;
-	function processkey(e){
+	function processkey(e) {
 	switch (e.keyCode) {
 		case 37:
 		case 65:
@@ -61,33 +71,29 @@ function canvasApp() {
 				dx = -snakeLength;
 				dy = 0;
 			}
-			snakeBodyMove();
 			break;
 		case 38:
 		case 87:
 			if(dy!=snakeHeight)
 			{dy = -snakeHeight;
 			dx = 0;}
-			snakeBodyMove();
 			break;
 		case 39:
 		case 68:
 			if(dx!=-snakeLength)
 			{dx = snakeLength;
 			dy = 0;}
-			snakeBodyMove();
 			break;
 		case 40:
 		case 83:
 			if(dy!=-snakeHeight)
 			{dy = snakeHeight;
 			dx = 0;}
-			snakeBodyMove();
 			break;
 			}
 	}		
 	
-	function boundingBoxCollide(){
+	function boundingBoxCollide() {
 		var left1 = 0;
 		var left2 = x;
 		var right1 = canvas.width;
@@ -106,6 +112,7 @@ function canvasApp() {
 			return false;
 		return true;
 	}
+
 	function drawGrid(context, color, stepx, stepy) {
 		context.clearRect(0, 0, canvas.width, canvas.height);
 		context.save();
@@ -115,10 +122,10 @@ function canvasApp() {
 		for (var i = stepx + 0.5; i <canvas.width; i += stepx) {
 			context.beginPath();
 			context.moveTo(i, 0);
-			context.lineTo(i,canvas.height);
+			context.lineTo(i, canvas.height);
 			context.stroke();
 			context.closePath();
-			}
+		}
 
 		for (var i = stepy + 0.5; i < canvas.height; i += stepy) {
 			context.beginPath();
@@ -126,8 +133,8 @@ function canvasApp() {
 			context.lineTo(canvas.width, i);
 			context.stroke();
 			context.closePath();
-			}
-			context.restore();
+		}
+		context.restore();
 	}
 	
 	function gameover(){
